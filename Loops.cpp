4fe0f3a90d1c27c3081e9d917dbc52f34c954ba5
j-portillo1 <144365700+j-portillo1@users.noreply.github.com>
2023-10-08 23:20:28 -0400

    Create Loops.cpp

---
+++ Loops.cpp
@@ -0,0 +1,25 @@
+// FOR LOOP
+//Modified from W3 Schools https://www.w3schools.com/cpp/cpp_for_loop.asp
+for (int i = 0; i < 10; i++) {
+  cout << i << "\n";
+}
+
+
+//WHILE LOOP
+//Modified from https://www.w3schools.com/cpp/cpp_while_loop.asp
+int i = 0;
+while (i < 10) {
+  cout << i << "\n";
+  i++;
+}
+
+
+
+//DO/WHILE LOOP
+//Modified from https://www.w3schools.com/cpp/cpp_do_while_loop.asp
+int i = 0;
+do {
+  cout << i << "\n";
+  i++;
+}
+while (i < 10);

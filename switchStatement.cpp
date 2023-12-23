07b462a8af6d9ee53e14cee879abccbd74a13425
j-portillo1 <144365700+j-portillo1@users.noreply.github.com>
2023-12-22 19:15:56 -0500

    Create switchStatement.cpp

---
+++ switchStatement.cpp
@@ -0,0 +1,12 @@
+int month = 5;
+switch (month) {
+  case 6:
+	cout << "The month is June";
+    break;
+  case 7:
+	cout << "The month is July";
+    break;
+  default:
+	cout << "Looking forward to the Summer! ";
+}
+// Outputs "Looking forward to the Summer! "

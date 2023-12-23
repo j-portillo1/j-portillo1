991f70b45b714e29310887f6d8b6804cb9c7d8c3
j-portillo1 <144365700+j-portillo1@users.noreply.github.com>
2023-12-22 19:16:54 -0500

    Create short-circuiting.cpp

---
+++ short-circuiting.cpp
@@ -0,0 +1,22 @@
+//AND
+
+if (0 == 1 && 1 + 1 == 2) {
+         cout <<("(0 == 1 && 1 + 1 == 2) is true\n");
+      } 
+      else {
+         cout<<("(0 == 1 && 1 + 1 == 2) is false\n");
+      }
+      
+//Returns (0 == 1 && 1 + 1 == 2) is false
+
+
+
+//OR
+if (1 + 1 == 2 || 0 == 1) {
+         cout <<("(1 + 1 == 2 || 0 == 1) is true\n");
+         } 
+         else {
+         cout <<("(1 + 1 == 2 || 0 == 1) is false\n");
+      }
+
+//Returns (1 + 1 == 2 || 0 == 1) is true
